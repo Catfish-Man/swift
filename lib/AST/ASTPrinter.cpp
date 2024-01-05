d26051b1adfbf5109ce4723948390db5e6b209ce
Pavel Yaskevich <pyaskevich@apple.com>
2024-01-05 13:45:28 -0800

    [AST] Support `@preconcurrency` attribute on test/extension inheritance entries

--- lib/AST/ASTPrinter.cpp
+++ lib/AST/ASTPrinter.cpp
@@ -2732,6 +2732,8 @@ void PrintAST::printInherited(const Decl *decl) {
     if (inherited.isRetroactive &&
         !llvm::is_contained(Options.ExcludeAttrList, TAK_retroactive))
       Printer << "@retroactive ";
+    if (inherited.isPreconcurrency)
+      Printer << "@preconcurrency ";
 
     printTypeLoc(inherited);
   }, [&]() {
@@ -8317,7 +8319,8 @@ swift::getInheritedForPrinting(
 
     Results.push_back({TypeLoc::withoutLoc(proto->getDeclaredInterfaceType()),
                        isUnchecked,
-                       /*isRetroactive=*/false});
+                       /*isRetroactive=*/false,
+                       /*isPreconcurrency=*/false});
   }
 }
 
